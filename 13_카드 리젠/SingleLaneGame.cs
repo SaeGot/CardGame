f9605d9755f5d4377bd8ed44c36cc603a4e6b987
SaeGot <saegot@naver.com>
2021-11-22 20:51:48 +0900

    카드 리젠 추가

---
+++ 13_카드 리젠/SingleLaneGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleLaneGame : MonoBehaviour
+{
+    public SingleLanePlayer singleLanePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetHand();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void SetHand()
+    {
+        singleLanePlayer.SetHand();
+    }
+}

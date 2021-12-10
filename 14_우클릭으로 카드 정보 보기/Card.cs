857781f51e2f3b5ebeca2a1d8127ab087603f08c
SaeGot <saegot@naver.com>
2021-12-10 19:47:01 +0900

    우클릭으로 카드 정보 보기

---
+++ 14_우클릭으로 카드 정보 보기/Card.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour, IPointerClickHandler
+{
+    public int cardType;
+    public GameObject infoText;
+    private GameObject instanceInfoText;
+
+    void Awake()
+    {
+        instanceInfoText = Instantiate(infoText, transform);
+        Vector2 position = new Vector2(0, 100);
+        instanceInfoText.transform.localPosition = position;
+        instanceInfoText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Down");
+            instanceInfoText.SetActive(false);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData event_Data)
+    {
+        if (event_Data.button == PointerEventData.InputButton.Right)
+        {
+            Debug.Log("Click");
+            instanceInfoText.GetComponent<Text>().text = GetInfo();
+            instanceInfoText.SetActive(true);
+        }
+    }
+
+    // 카드 정보 보여주기
+    public void ShowInfo(GameObject card_Instance)
+    {
+        instanceInfoText = Instantiate(infoText, card_Instance.transform);
+        Vector2 position = new Vector2(0, 100);
+        instanceInfoText.transform.localPosition = position;
+        infoText.GetComponent<Text>().text = GetInfo();
+    }
+
+    // 카드 정보 가져오기
+    public string GetInfo()
+    {
+        switch (cardType)
+        {
+            case 0:
+                return "적의 공격을 방어합니다.";
+            case 1:
+                return "적에게 1의 대미지를 줍니다.";
+            case 2:
+                return "적에게 2의 대미지를 줍니다.";
+            case 3:
+                return "적에게 3의 대미지를 줍니다.";
+        }
+
+        return "";
+    }
+}
---
+++ 14_우클릭으로 카드 정보 보기/SingleLanePlayer.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SingleLanePlayer : MonoBehaviour
+{
+    public GameObject card;
+    public GameObject canvas;
+    SingleLaneElement singleLaneElement;
+
+    void Awake()
+    {
+        singleLaneElement = new SingleLaneElement();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ClickCard(GameObject card_Instance)
+    {
+        string selected_card_name = EventSystem.current.currentSelectedGameObject.name;
+        singleLaneElement.selectedCard = selected_card_name;
+        card_Instance.GetComponent<Card>().ShowInfo(card_Instance);
+        Debug.Log(selected_card_name + " Selected");
+    }
+
+    public void ClickEndTurn()
+    {
+        if (singleLaneElement.selectedCard != "")
+        {
+            string selected_card_name = singleLaneElement.selectedCard;
+            GameObject selected_object = GameObject.Find(selected_card_name);
+            // 선택된 카드 이동
+            selected_object.transform.position = new Vector3(0, 0, 0);
+            Debug.Log(selected_card_name + " Moved to (0,0,0)");
+            // 선택된 카드의 타입 가져오기
+            GameObject text_object = selected_object.transform.Find("Text").gameObject;
+            int card_damage = int.Parse(text_object.GetComponent<Text>().text);
+            // 데미지
+            singleLaneElement.life -= card_damage;
+            GameObject.Find("Score").GetComponent<Text>().text = singleLaneElement.life.ToString();
+            Debug.Log("Life damaged " + card_damage);
+            Debug.Log("Current Life " + singleLaneElement.life);
+            // 선택된 카드 손에서 제거
+            string card_num = selected_card_name.Substring(selected_card_name.Length - 1);
+            singleLaneElement.handCard.Remove(int.Parse(card_num));
+            // 손에 남은 카드 개수
+            int num_of_remain_cards = singleLaneElement.handCard.Count;
+            Debug.Log("Card ID = " + card_num + " removed from hand");
+            Debug.Log(num_of_remain_cards + " Cards remained at hand");
+
+            string object_name = selected_object.name;
+            try
+            {
+                Destroy(selected_object);
+                Debug.Log(object_name + " : Destroyed");
+            }
+            catch
+            {
+                Debug.LogError(object_name + " : Destroying Failed");
+            }
+            // 손에 카드가 한장이면 손 털기
+            if (num_of_remain_cards <= 1)
+            {
+                ClearHand();
+                SetHand();
+            }
+
+            singleLaneElement.selectedCard = "";
+        }
+        else
+        {
+            Debug.Log("Card not selected");
+        }
+    }
+
+    public void SetHand()
+    {
+        singleLaneElement.SetHand();
+        Vector2 position = new Vector2(0, -200);
+        int position_x = -3 * 250;
+        foreach (var item in singleLaneElement.handCard)
+        {
+            GameObject temp = Instantiate(card, canvas.transform);
+            //현재 오브젝트 위치 설정
+            position_x += 250;
+            position.x = position_x;
+            temp.transform.localPosition = position;
+            //카드 타입 설정
+            temp.GetComponent<Card>().cardType = item.Value;
+            //텍스트 설정
+            temp.transform.Find("Text").GetComponent<Text>().text = item.Value.ToString();
+            //오브젝트명 변경
+            string card_name = "Card_" + item.Key;
+            temp.name = card_name;
+            //버튼클릭 설정
+            temp.GetComponent<Button>().onClick.AddListener(delegate { ClickCard(temp); });
+
+            Debug.Log(card_name + " Created at x :" + position_x.ToString());
+        }
+    }
+
+    // 손 털기
+    public void ClearHand()
+    {
+        string card_name;
+        string object_name;
+        foreach (var item in singleLaneElement.handCard)
+        {
+            card_name = "Card_" + item.Key;
+            GameObject game_object = GameObject.Find(card_name).gameObject;
+            object_name = game_object.name;
+            // 카드 오브젝트 제거
+            try
+            {
+                Destroy(game_object);
+                Debug.Log(object_name + " : Destroyed");
+            }
+            catch
+            {
+                Debug.LogError(object_name + " : Destroying Failed");
+            }
+        }
+        // 손에서 카드 털기
+        singleLaneElement.ClearHand();
+        Debug.Log("Hand Cleared");
+    }
+}

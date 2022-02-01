80fe1e7d3647c85bfe242c1590d9123871663a16
SaeGot <saegot@naver.com>
2022-02-01 17:25:11 +0900

    16 상대 추가

---
+++ 16_상대 추가/SingleLaneElement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleLaneElement
+{
+    public string selectedCard;
+    public Dictionary<int, int> handCard;
+    public int life;
+    public List<int> startingCardList;
+
+    public SingleLaneElement(int _life)
+    {
+        selectedCard = "";
+        handCard = new Dictionary<int, int>();
+        life = _life;
+        startingCardList = new List<int>(new int[] { 0, 1, 2, 2, 3 });
+    }
+
+    // 카드 핸드에 세팅
+    public void SetHand()
+    {
+        for (int n = 0; n < startingCardList.Count; n++)
+        {
+            int card_kind = startingCardList[n];
+            handCard.Add(n, card_kind);
+        }
+    }
+
+    // 손에서 카드 털기
+    public void ClearHand()
+    {
+        handCard.Clear();
+    }
+}
---
+++ 16_상대 추가/SingleLaneGame.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SingleLaneGame : MonoBehaviour
+{
+    public SingleLanePlayer me;
+    public SingleLanePlayer you;
+    bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        me.Initialize(false);
+        you.Initialize(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 턴 종료 클릭
+    public void ClickEndTurn()
+    {
+        if (!gameOver)
+        {
+            if (me.CheckSelectedCard())
+            {
+                SetButton(false, false);                
+                StartCoroutine("Battle");
+            }
+            else
+            {
+                Debug.Log("Card not selected");
+            }
+        }
+    }
+
+    // 전투
+    private IEnumerator Battle()
+    {        
+        you.AISelectCard();
+        me.Ready();
+        you.Ready();
+        yield return new WaitForSeconds(1f);
+
+        int me_damage = me.GetDamage(you);
+        int you_damage = you.GetDamage(me);
+        me.Fight(you_damage);
+        you.Fight(me_damage);
+        yield return new WaitForSeconds(0.5f);
+
+        CheckGameOver();
+        SetButton(true, true);
+        yield return null;
+    }
+
+    // 게임종료 확인
+    private bool CheckGameOver()
+    {
+        int me_life = me.GetLife();
+        int you_life = you.GetLife();
+
+        if (me_life <= 0 || you_life <= 0)
+        {
+            GameObject canvas = GameObject.Find("Canvas");
+            canvas.transform.Find("GameOver").gameObject.SetActive(true);
+            gameOver = true;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // 버튼 활성 설정
+    private void SetButton(bool left_ClickEnabled, bool right_ClickEnabled)
+    {
+        GameObject.Find("EndTurn").GetComponent<Button>().interactable = left_ClickEnabled;
+        List<int> remain_cards = me.GetRemainCards();
+        GameObject me_object = GameObject.Find("Me");
+        foreach (var card in remain_cards)
+        {
+            string card_name = "Card_" + card.ToString();
+            GameObject card_object = me_object.transform.Find(card_name).gameObject;
+            card_object.GetComponent<Button>().interactable = left_ClickEnabled;
+            card_object.GetComponent<Card>().InfoTextSetActive(false, right_ClickEnabled);
+        }
+    }
+}
